9cd8fc1351c4512031d54670c7570d3c1f781d54
Roger Ramirez <roger.ramirez@sap.com>
2021-11-21 20:36:25 -0500

    Oscar Open mp

---
+++ openmp_oscar/cmake-build-debug/CMakeFiles/test6.dir/DependInfo.cmake
@@ -0,0 +1,84 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zinobe/CLionProjects/test6/main.cpp" "/home/zinobe/CLionProjects/test6/cmake-build-debug/CMakeFiles/test6.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/zinobe/opencv_build/opencv/build"
+  "/home/zinobe/opencv_build/opencv/include"
+  "/home/zinobe/opencv_build/opencv/modules/core/include"
+  "/home/zinobe/opencv_build/opencv/modules/flann/include"
+  "/home/zinobe/opencv_build/opencv/modules/imgproc/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/intensity_transform/include"
+  "/home/zinobe/opencv_build/opencv/modules/ml/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/phase_unwrapping/include"
+  "/home/zinobe/opencv_build/opencv/modules/photo/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/plot/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/quality/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/reg/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/surface_matching/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/xphoto/include"
+  "/home/zinobe/opencv_build/opencv/modules/dnn/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/dnn_superres/include"
+  "/home/zinobe/opencv_build/opencv/modules/features2d/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/freetype/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/fuzzy/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/hfs/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/img_hash/include"
+  "/home/zinobe/opencv_build/opencv/modules/imgcodecs/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/line_descriptor/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/saliency/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/text/include"
+  "/home/zinobe/opencv_build/opencv/modules/videoio/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/wechat_qrcode/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/barcode/include"
+  "/home/zinobe/opencv_build/opencv/modules/calib3d/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/datasets/include"
+  "/home/zinobe/opencv_build/opencv/modules/highgui/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/mcc/include"
+  "/home/zinobe/opencv_build/opencv/modules/objdetect/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/rapid/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/rgbd/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/shape/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/structured_light/include"
+  "/home/zinobe/opencv_build/opencv/modules/ts/include"
+  "/home/zinobe/opencv_build/opencv/modules/video/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/videostab/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/xfeatures2d/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/ximgproc/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/xobjdetect/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/aruco/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/bgsegm/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/bioinspired/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/ccalib/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/dnn_objdetect/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/dpm/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/face/include"
+  "/home/zinobe/opencv_build/opencv/modules/gapi/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/optflow/include"
+  "/home/zinobe/opencv_build/opencv/modules/stitching/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/superres/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/tracking/include"
+  "/home/zinobe/opencv_build/opencv_contrib/modules/stereo/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ openmp_oscar/cmake-build-debug/CMakeFiles/test6.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test6.dir/main.cpp.o"
+  "test6"
+  "test6.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
